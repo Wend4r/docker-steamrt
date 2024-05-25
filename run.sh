a2b4d602dbfbfd94a8580b3aa3f8d23f8ee0bf3c
Vladimir <47463683+Wend4r@users.noreply.github.com>
2024-05-25 19:34:25 +0300

    Run script: split the arguments to next lines

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ export STEAMRT_SNIPER_IMAGE_NAME=steamrt-sniper
 export STEAMRT_SNIPER_CONTAINER_NAME=steamrt-sniper
 export STEAMRT_SNIPER_HOSTNAME=steamrt-sniper
 
-docker run -it --hostname "$STEAMRT_SNIPER_HOSTNAME" --restart unless-stopped --name "$STEAMRT_SNIPER_CONTAINER_NAME" "$STEAMRT_SNIPER_IMAGE_NAME"
+docker run -it \
+           --hostname "$STEAMRT_SNIPER_HOSTNAME" \
+           --restart unless-stopped \
+           --name "$STEAMRT_SNIPER_CONTAINER_NAME" \
+           "$STEAMRT_SNIPER_IMAGE_NAME"
