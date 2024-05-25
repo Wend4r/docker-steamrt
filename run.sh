2c26cce74b0e4912e931283669247181160e54ff
Vladimir <47463683+Wend4r@users.noreply.github.com>
2024-05-25 19:32:58 +0300

    Run script: add `STEAMRT_SNIPER_IMAGE_NAME` environment variable

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+export STEAMRT_SNIPER_IMAGE_NAME=steamrt-sniper
 export STEAMRT_SNIPER_CONTAINER_NAME=steamrt-sniper
 export STEAMRT_SNIPER_HOSTNAME=steamrt-sniper
 
-docker run -it --hostname "$STEAMRT_SNIPER_HOSTNAME" --restart unless-stopped "$STEAMRT_SNIPER_CONTAINER_NAME"
+docker run -it --hostname "$STEAMRT_SNIPER_HOSTNAME" --restart unless-stopped --name "$STEAMRT_SNIPER_CONTAINER_NAME" "$STEAMRT_SNIPER_IMAGE_NAME"
